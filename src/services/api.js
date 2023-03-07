7b06dbff4c6d97dbee4aa997682e4e645f560b3d
Rajaneesh Gudivada <rajaneesh1n2345@gmail.com>
2023-03-07 10:39:21 +0530

    added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // filters of type: {advancedOptionsKey[string]: optionValue[Set<string>]}
 
-const url = 'http://localhost:5000';
-// const url = 'https://aaokbackend.onrender.com';
+// const url = 'http://localhost:5000';
+const url = 'https://aaokbackend.onrender.com';
 
 const getSearchResults = async (filters, sortOption) => {
 	// console.log('getSearchResults', filters);
